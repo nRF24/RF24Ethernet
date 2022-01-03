88820be1b54033987b60aaea7ecb13d42b86c25f
TMRh20 <tmrh20@gmail.com>
2022-01-03 10:35:42 -0600

    Update clock-arch.c

--- clock-arch.c
+++ clock-arch.c
@@ -18,7 +18,9 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#if !defined ARDUINO_ARCH_MBED_RP2040
 #include <Arduino.h>
+#endif
 #include "clock-arch.h"
 
 clock_time_t clock_time(void)
